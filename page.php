ff50b9c58e4cec874346fa2295fbdf9d746c3e91
Cons Bulaquena <consbulaquena@gmail.com>
2020-11-10 07:55:09 +0800

    add page.php

---
+++ page.php
@@ -0,0 +1,12 @@
+<?php
+    //haveposts question is there available post?
+    //the posts() it enables other posts related function 
+
+    while(have_posts()) {
+        the_post(); 
+        ?>
+        <h2><?php the_title(); ?></h2>
+        <div><?php the_content(); ?></div>
+    <?php }
+
+?>

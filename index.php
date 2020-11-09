e8f9c0654babc0ca65df467217bad5e593fb7c55
Cons Bulaquena <consbulaquena@gmail.com>
2020-11-10 01:28:32 +0800

    intial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    //haveposts question is there available post?
+    //the posts() it enables other posts related function 
+
+    while(have_posts()) {
+        the_post(); 
+        ?>
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></h2></a>
+        <div><?php the_content(); ?></div>
+    <?php }
+
+?>

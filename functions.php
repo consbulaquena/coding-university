659d4e22d9d33068d9efab347aa6b17d4d76a5ab
Cons Bulaquena <consbulaquena@gmail.com>
2020-11-10 09:36:10 +0800

    add icons

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
 function university_files() {
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('university_main_styles', get_stylesheet_uri());
 }
 

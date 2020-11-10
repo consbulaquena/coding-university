611714f55bb699a5a7417f3c9b96c8525deb7583
Cons Bulaquena <consbulaquena@gmail.com>
2020-11-10 09:57:39 +0800

    add css, javascript, images

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 
 <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(images/library-hero.jpg);"></div>
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>);"></div>
       <div class="page-banner__content container t-center c-white">
         <h1 class="headline headline--large">Welcome!</h1>
         <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>

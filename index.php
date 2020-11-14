268678a8f5ebd1f1fa43dbe018ad0636280974a5
Cons Bulaquena <consbulaquena@gmail.com>
2020-11-14 19:44:18 +0800

    edit html content

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <div class="full-width-split group">
       <div class="full-width-split__one">
         <div class="full-width-split__inner">
-          <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
+          <h2 class="headline headline--small-plus t-center">Our Bootcamps</h2>
 
           <div class="event-summary">
             <a class="event-summary__date t-center" href="#">
@@ -22,8 +22,8 @@
               <span class="event-summary__day">25</span>
             </a>
             <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">Poetry in the 100</a></h5>
-              <p>Bring poems you&rsquo;ve wrote to the 100 building this Tuesday for an open mic and snacks. <a href="#" class="nu gray">Learn more</a></p>
+              <h5 class="event-summary__title headline headline--tiny"><a href="#">Onsite 12 Weeks Full-Time</a></h5>
+              <p>Join Coding Bootcamp’s software engineering program at one of the Galvanize campuses across the nation. <a href="#" class="nu gray">Learn more</a></p>
             </div>
           </div>
           <div class="event-summary">
@@ -32,12 +32,12 @@
               <span class="event-summary__day">02</span>
             </a>
             <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">Quad Picnic Party</a></h5>
-              <p>Live music, a taco truck and more can found in our third annual quad picnic day. <a href="#" class="nu gray">Learn more</a></p>
+              <h5 class="event-summary__title headline headline--tiny"><a href="#">Remote 12 Weeks Full-Time (Online)</a></h5>
+              <p>Reinvent your career remotely with our live online immersive coding bootcamp, which directly mirrors our in-person program. <a href="#" class="nu gray">Learn more</a></p>
             </div>
           </div>
 
-          <p class="t-center no-margin"><a href="#" class="btn btn--blue-outline">View All Events</a></p>
+          <p class="t-center no-margin"><a href="#" class="btn btn--blue-outline">Learn More</a></p>
         </div>
       </div>
       <div class="full-width-split__two">

2a5456b1bb56527324542cb62854ce9b6044154a
Cons Bulaquena <consbulaquena@gmail.com>
2020-11-16 05:01:50 +0800

    add site url

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
         <div class="site-header__menu group">
           <nav class="main-navigation">
             <ul>
-              <li><a href="#">About Us</a></li>
+              <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>

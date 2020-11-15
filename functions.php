ff6dbe9d0f2af5b6f18beb6fd086d49197f2ea63
Cons Bulaquena <consbulaquena@gmail.com>
2020-11-16 02:30:29 +0800

    add action change title

--- functions.php
+++ functions.php
@@ -17,3 +17,9 @@ function university_files() {
 //add_action=hook
 // this is RUN
 add_action('wp_enqueue_scripts', 'university_files');
+
+function university_features() {
+    add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'university_features');

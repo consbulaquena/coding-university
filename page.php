2b938978cee85441b62d4faf51a33e3cca18eaba
Cons Bulaquena <consbulaquena@gmail.com>
2020-11-16 08:15:02 +0800

    add if statement for get post parent id

--- page.php
+++ page.php
@@ -21,9 +21,10 @@
   <div class="container container--narrow page-section">
 
       <?php
-      if (wp_get_post_parent_id(get_the_ID())) { ?>
+      $theParent = wp_get_post_parent_id(get_the_ID());
+      if ($theParent) { ?>
       <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title(); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
+      <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
     </div>
         <?php }
       ?>

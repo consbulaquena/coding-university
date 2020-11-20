571f5a9806327cc4238ea78a8ef28841703e0fec
Cons Bulaquena <consbulaquena@gmail.com>
2020-11-20 15:21:30 +0800

    add sort_column

--- page.php
+++ page.php
@@ -29,9 +29,15 @@
         <?php }
       ?>
 
+        <?php 
+        $testArray = get_pages(array(
+          'child_of' => get_the_ID()
 
+        ));
+        
+        if ($theParent or $testArray) { ?>
     <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
+      <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
       <ul class="min-list">
         <?php
           if ($theParent) {
@@ -42,12 +48,13 @@
           
           wp_list_pages(array(
             'title_li' => NULL,
-            'child_of' => $findChildrenOf
+            'child_of' => $findChildrenOf,
+            'sort_column' => 'menu_order'
           ));
         ?>
       </ul>
     </div>
-
+        <?php } ?>
 
     <div class="generic-content">
         <?php the_content(); ?>

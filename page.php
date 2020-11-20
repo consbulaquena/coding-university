b7fa3ae5332165c50732a65c1b2cf98d50363aad
Cons Bulaquena <consbulaquena@gmail.com>
2020-11-20 14:52:49 +0800

    fix the menu

--- page.php
+++ page.php
@@ -29,15 +29,25 @@
         <?php }
       ?>
 
-    <!--
+
     <div class="page-links">
       <h2 class="page-links__title"><a href="#">About Us</a></h2>
       <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
+        <?php
+          if ($theParent) {
+            $findChildrenOf = $theParent;
+          } else {
+            $findChildrenOf = get_the_ID();
+          }
+          
+          wp_list_pages(array(
+            'title_li' => NULL,
+            'child_of' => $findChildrenOf
+          ));
+        ?>
       </ul>
     </div>
-    -->
+
 
     <div class="generic-content">
         <?php the_content(); ?>

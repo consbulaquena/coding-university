6a40db10260e5e6c309d5f5aed20f59b4a2418d4
Cons Bulaquena <consbulaquena@gmail.com>
2020-11-20 19:50:25 +0800

    Build the blog in index.php

---
+++ front-page.php
@@ -0,0 +1,110 @@
+<?php get_header(); ?>
+
+
+<div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>);"></div>
+      <div class="page-banner__content container t-center c-white">
+        <h1 class="headline headline--large"></h1>
+        <h2 class="headline headline--medium">Software Engineering Programs</h2>
+        <h3 class="headline headline--small">Coding Bootcamp online courses will teach you the javascript skills to become a Software Engineer.</h3>
+        <a href="#" class="btn btn--large btn--blue">Enroll now</a>
+      </div>
+    </div>
+
+    <div class="full-width-split group">
+      <div class="full-width-split__one">
+        <div class="full-width-split__inner">
+          <h2 class="headline headline--small-plus t-center">Our Bootcamps</h2>
+
+          <div class="event-summary">
+            <a class="event-summary__date t-center" href="#">
+              <span class="event-summary__month">Mar</span>
+              <span class="event-summary__day">25</span>
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="#">Onsite 12 Weeks Full-Time</a></h5>
+              <p>Join Coding Bootcamp’s software engineering program at one of the Galvanize campuses across the nation. <a href="#" class="nu gray">Learn more</a></p>
+            </div>
+          </div>
+          <div class="event-summary">
+            <a class="event-summary__date t-center" href="#">
+              <span class="event-summary__month">Apr</span>
+              <span class="event-summary__day">02</span>
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="#">Remote 12 Weeks Full-Time (Online)</a></h5>
+              <p>Reinvent your career remotely with our live online immersive coding bootcamp, which directly mirrors our in-person program. <a href="#" class="nu gray">Learn more</a></p>
+            </div>
+          </div>
+
+          <p class="t-center no-margin"><a href="#" class="btn btn--blue-outline">Learn More</a></p>
+        </div>
+      </div>
+      <div class="full-width-split__two">
+        <div class="full-width-split__inner">
+          <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
+
+          <div class="event-summary">
+            <a class="event-summary__date event-summary__date--beige t-center" href="#">
+              <span class="event-summary__month">Jan</span>
+              <span class="event-summary__day">20</span>
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="#">We Were Voted Best School</a></h5>
+              <p>For the 100th year in a row we are voted #1. <a href="#" class="nu gray">Read more</a></p>
+            </div>
+          </div>
+          <div class="event-summary">
+            <a class="event-summary__date event-summary__date--beige t-center" href="#">
+              <span class="event-summary__month">Feb</span>
+              <span class="event-summary__day">04</span>
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="#">Professors in the National Spotlight</a></h5>
+              <p>Two of our professors have been in national news lately. <a href="#" class="nu gray">Read more</a></p>
+            </div>
+          </div>
+
+          <p class="t-center no-margin"><a href="#" class="btn btn--blue-outline">View All Blog Posts</a></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="hero-slider">
+      <div data-glide-el="track" class="glide__track">
+        <div class="glide__slides">
+          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('images/bus.jpg') ?>);">
+            <div class="hero-slider__interior container">
+              <div class="hero-slider__overlay">
+                <h2 class="headline headline--medium t-center">JAVASCRIPT FOR BEGINNERS</h2>
+                <p class="t-center">This online, part-time course is a perfect way to kick off your coding education.</p>
+                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('images/apples.jpg') ?>);">
+            <div class="hero-slider__interior container">
+              <div class="hero-slider__overlay">
+                <h2 class="headline headline--medium t-center">CS PREP</h2>
+                <p class="t-center">CS Prep teaches core JavaScript concepts through live-online instruction.</p>
+                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('images/bread.jpg') ?>);">
+            <div class="hero-slider__interior container">
+              <div class="hero-slider__overlay">
+                <h2 class="headline headline--medium t-center">FREE CODING WORKSHOPS</h2>
+                <p class="t-center">Coding bootcamp break down critical coding concepts for all.</p>
+                <p class="t-center no-margin"><a href="#" class="btn btn--blue">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="slider__bullets glide__bullets" data-glide-el="controls[nav]"></div>
+      </div>
+    </div>
+
+    <?php
+    get_footer();
+?>
--- header.php
+++ header.php
@@ -15,20 +15,14 @@
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
           <nav class="main-navigation">
-          <?php
-            wp_nav_menu(array(
-              'theme_location' => 'headerMenuLocation'
-            ));
-            ?>
 
-            <!-- <ul>
-              <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+              <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 12) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
               <li><a href="#">Blog</a></li>
             </ul>
--->
+
           </nav>
           <div class="site-header__util">
             <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>

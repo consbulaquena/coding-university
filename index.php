1c7d6527282ee89e9b31fd59e7b236765b3bd719
Cons Bulaquena <consbulaquena@gmail.com>
2020-11-21 10:26:12 +0800

    add class

--- index.php
+++ index.php
@@ -17,10 +17,11 @@ get_header(); ?>
   while(have_posts()) {
     the_post(); ?>
     <div class="post-item">
-      <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <h2 class="headline headline--medium headline--post-title">
+      <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
     <div class="meta-box">
-    <p>Posted by Brad on 6.8.17 in News </p>
+    <p>Posted by Cons on 6.8.17 in News </p>
     </div>
 
     <div class="generic-content">

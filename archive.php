1e6f20d9cf7db20d73968240194f92684e3d0eaf
Cons Bulaquena <consbulaquena@gmail.com>
2020-11-23 15:49:59 +0800

    add if statement

--- archive.php
+++ archive.php
@@ -5,7 +5,12 @@ get_header(); ?>
 <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/blue.jpg') ?>);"></div>
     <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title">Welcome to Our Blog!</h1>
+      <h1 class="page-banner__title"><?php if (is_category()) {
+       echo "category name will go here";  
+      } 
+      if (is_author()) {
+        echo "author name"; 
+        } ?></h1>
       <div class="page-banner__intro">
         <p>Keep up with our latest news.</p>
       </div>

b51433f4347f721d1fa9b75f7c48727eee44c000
Cons Bulaquena <consbulaquena@gmail.com>
2020-11-24 20:28:22 +0800

    add homepagePosts

--- archive.php
+++ archive.php
@@ -5,14 +5,9 @@ get_header(); ?>
 <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/blue.jpg') ?>);"></div>
     <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php if (is_category()) {
-       echo "category name will go here";  
-      } 
-      if (is_author()) {
-        echo "author name"; 
-        } ?></h1>
+      <h1 class="page-banner__title"><?php the_archive_title(); ?></h1>
       <div class="page-banner__intro">
-        <p>Keep up with our latest news.</p>
+        <p><?php the_archive_description(); ?></p>
       </div>
     </div>  
   </div>
--- front-page.php
+++ front-page.php
@@ -43,10 +43,17 @@
       <div class="full-width-split__two">
         <div class="full-width-split__inner">
           <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
+          <?php 
+            $homepagePosts = new WP_Query(array(
+              'posts_per_page' => 2
+            ));
 
-          <div class="event-summary">
+            while ($homepagePosts->have_posts()) {
+              $homepagePosts->the_post(); ?>
+              
+              <div class="event-summary">
             <a class="event-summary__date event-summary__date--beige t-center" href="#">
-              <span class="event-summary__month">Jan</span>
+              <span class="event-summary__month"><?php the_time(); ?></span>
               <span class="event-summary__day">20</span>
             </a>
             <div class="event-summary__content">
@@ -54,16 +61,14 @@
               <p>For the 100th year in a row we are voted #1. <a href="#" class="nu gray">Read more</a></p>
             </div>
           </div>
-          <div class="event-summary">
-            <a class="event-summary__date event-summary__date--beige t-center" href="#">
-              <span class="event-summary__month">Feb</span>
-              <span class="event-summary__day">04</span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">Professors in the National Spotlight</a></h5>
-              <p>Two of our professors have been in national news lately. <a href="#" class="nu gray">Read more</a></p>
-            </div>
-          </div>
+
+              <?php 
+            }
+          ?>
+
+
+
+          
 
           <p class="t-center no-margin"><a href="#" class="btn btn--blue-outline">View All Blog Posts</a></p>
         </div>

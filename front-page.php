8f0f0d860aa14b97c5d3ed57b8f4643364cd6928
Cons Bulaquena <consbulaquena@gmail.com>
2020-11-24 20:37:35 +0800

    echo trim content

--- front-page.php
+++ front-page.php
@@ -58,7 +58,7 @@
             </a>
             <div class="event-summary__content">
               <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-              <p>For the 100th year in a row we are voted #1. <a href="#" class="nu gray">Read more</a></p>
+              <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="#" class="nu gray">Read more</a></p>
             </div>
           </div>
 

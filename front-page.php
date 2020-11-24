5573f8fe3032e445470239074b88239d54736c58
Cons Bulaquena <consbulaquena@gmail.com>
2020-11-24 20:33:43 +0800

    add permalink, edit content

--- front-page.php
+++ front-page.php
@@ -53,11 +53,11 @@
               
               <div class="event-summary">
             <a class="event-summary__date event-summary__date--beige t-center" href="#">
-              <span class="event-summary__month"><?php the_time(); ?></span>
-              <span class="event-summary__day">20</span>
+              <span class="event-summary__month"><?php the_time('M'); ?></span>
+              <span class="event-summary__day"><?php the_time('d'); ?></span>
             </a>
             <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">We Were Voted Best School</a></h5>
+              <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
               <p>For the 100th year in a row we are voted #1. <a href="#" class="nu gray">Read more</a></p>
             </div>
           </div>

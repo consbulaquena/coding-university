6487123d7a8308b25933336f1f3da8ac47c07226
Cons Bulaquena <consbulaquena@gmail.com>
2020-11-27 10:54:15 +0800

    updating link

--- front-page.php
+++ front-page.php
@@ -52,25 +52,25 @@
               $homepagePosts->the_post(); ?>
               
               <div class="event-summary">
-            <a class="event-summary__date event-summary__date--beige t-center" href="#">
+            <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
               <span class="event-summary__month"><?php the_time('M'); ?></span>
               <span class="event-summary__day"><?php the_time('d'); ?></span>
             </a>
             <div class="event-summary__content">
               <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-              <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="#" class="nu gray">Read more</a></p>
+              <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
             </div>
           </div>
 
               <?php 
-            }
+            } wp_reset_postdata();
           ?>
 
 
 
           
 
-          <p class="t-center no-margin"><a href="#" class="btn btn--blue-outline">View All Blog Posts</a></p>
+          <p class="t-center no-margin"><a href="<?php echo site_url('/blog'); ?>" class="btn btn--blue-outline">View All Blog Posts</a></p>
         </div>
       </div>
     </div>

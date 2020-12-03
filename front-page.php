ce6bced044325c54e8bec76be5efaf92806a3769
Cons Bulaquena <consbulaquena@gmail.com>
2020-12-04 02:14:05 +0800

    add archive event

--- front-page.php
+++ front-page.php
@@ -29,8 +29,8 @@
               <span class="event-summary__day">25</span>
             </a>
             <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#"><?php the_title(); ?></a></h5>
-              <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="#" class="nu gray">Learn more</a></p>
+              <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+              <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
             </div>
           </div>
             <?php }
---
+++ university-post-types.php
@@ -0,0 +1,19 @@
+<?php
+
+function university_post_types() {
+    register_post_type('event', array(
+        'has_archive' => true,
+        'public' => true,
+        'labels' => array(
+            'name' => 'Events',
+            'add_new_item' => 'Add New Event',
+            'edit_item' => 'Edit Event',
+            'all_items' => 'All Events',
+            'singular_name' => 'Event'
+        ),
+        'menu_icon' => 'dashicons-calendar'
+    ));
+}
+
+add_action('init', 'university_post_types');
+

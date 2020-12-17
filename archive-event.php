5a0ebe6316a4226068ae94d2047be4be45333a9a
Cons Bulaquena <consbulaquena@gmail.com>
2020-12-17 10:24:37 +0800

    update event summary content

--- archive-event.php
+++ archive-event.php
@@ -5,9 +5,9 @@ get_header(); ?>
 <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/blue.jpg') ?>);"></div>
     <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title">All Events</h1>
+      <h1 class="page-banner__title">All Bootcamps</h1>
       <div class="page-banner__intro">
-        <p><?php the_archive_description(); ?></p>
+        <p>Check out our Bootcamps</p>
       </div>
     </div>  
   </div>
@@ -16,19 +16,18 @@ get_header(); ?>
 <?php
   while(have_posts()) {
     the_post(); ?>
-    <div class="post-item">
-    <h2 class="headline headline--medium headline--post-title">
-      <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+              <div class="event-summary">
+              <a class="event-summary__date t-center" href="#">
+                <span class="event-summary__month">Mar</span>
+                <span class="event-summary__day">25</span>
+              </a>
+              <div class="event-summary__content">
+                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+              </div>
+            </div>
 
-    <div class="meta-box">
-    <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n-F-y'); ?> in <?php echo get_the_category_list(', '); ?> </p>
-    </div>
 
-    <div class="generic-content">
-    <?php the_excerpt(); ?>
-    <p><a class="btn btn--blue-outline" href="<?php the_permalink(); ?>">Continue reading</a></p>
-    </div>
-  </div>
   <?php }
   echo paginate_links();
   ?>
--- front-page.php
+++ front-page.php
@@ -21,20 +21,20 @@
               'post_type' => 'event'
             ));
 
-            while($homepageEvents->have_posts()) {
-              $homepageEvents->the_post(); ?>
-            <div class="event-summary">
-            <a class="event-summary__date t-center" href="#">
-              <span class="event-summary__month">Mar</span>
-              <span class="event-summary__day">25</span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-              <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+              while($homepageEvents->have_posts()) {
+                $homepageEvents->the_post(); ?>
+              <div class="event-summary">
+              <a class="event-summary__date t-center" href="#">
+                <span class="event-summary__month">Mar</span>
+                <span class="event-summary__day">25</span>
+              </a>
+              <div class="event-summary__content">
+                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+              </div>
             </div>
-          </div>
-            <?php }
-          ?>
+              <?php }
+            ?>
 
 
 

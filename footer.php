0cec37dd35347c8688ebba190ced64350189b49c
Cons Bulaquena <consbulaquena@gmail.com>
2020-12-23 19:05:03 +0800

    add objects, events methods for search

--- footer.php
+++ footer.php
@@ -62,11 +62,12 @@
       </div>
     </footer>
 
-  <div class="search-overlay search-overlay--active">
+  <div class="search-overlay">
   <div class="search-overlay__top">
   <div class="container">
   <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
   <input type="text" class="search-term" placeholder="What are you searching for?" id="search-term">
+  <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
   </div>
   </div>  
   </div>
--- js/modules/Search.js
+++ js/modules/Search.js
@@ -1,7 +1,29 @@
+import $ from 'jquery';
+
 class Search {
+    // 1.describe and create our object
     constructor() {
-        alert("hello");
+        this.openButton = $(".js-search-trigger");
+        this.closeButton = $(".search-overlay__close");
+        this.searchOverlay = $(".search-overlay");
+        this.events();
     }
+
+    // 2. events
+    events() {
+        this.openButton.on("click", this.openOverlay.bind(this));
+        this.closeButton.on("click", this.closeOverlay.bind(this));
+    }
+
+    // 3. methods (function,action)
+    openOverlay() {
+        this.searchOverlay.addClass("search-overlay--active");
+    }
+
+    closeOverlay() {
+        this.searchOverlay.removeClass("search-overlay--active");
+    }
+
 }
 
 export default Search;
